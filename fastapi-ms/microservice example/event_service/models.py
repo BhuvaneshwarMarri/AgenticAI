312b3d80f016f2acad670544129cf85a459353db
BhuvaneshwarMarri <bhuvaneshwarmarri@gmail.com>
2026-01-28 02:31:25 +0530

    fast-api updated

---
+++ fastapi-ms/microservice example/event_service/models.py
@@ -0,0 +1,14 @@
+from sqlalchemy import Column, Integer, String, Date
+from event_service.config import Base, engine
+
+class Event(Base):
+    __tablename__ = "events"
+    
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100), nullable=False, index=True)
+    city= Column(String[50], nullable=False)
+    date = Column(Date, nullable=False)
+    organizer = Column(String(100), nullable=False)
+    organizer_mail = Column(String(100), nullable=True)
+    
+Base.metadata.create_all(bind=engine)
